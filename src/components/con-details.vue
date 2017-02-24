42c4561b168b7a66ebb62c16900c6301b71b985d
Samuel Colvin <s@muelcolvin.com>
2017-02-24 19:05:30 +0000

    spliting out con details into new component

---
+++ src/components/con-details.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <div class="tcs-location">
+      <!--
+      this is the svg for map icon straight from
+      https://github.com/encharm/Font-Awesome-SVG-PNG/blob/master/black/svg/map-marker.svg
+      -->
+      <svg class="tcs-svg" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1152 640q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm256 0q0 109-33
+          179l-364 774q-16 33-47.5 52t-67.5 19-67.5-19-46.5-52l-365-774q-33-70-33-179 0-212 150-362t362-150
+          362 150 150 362z"/>
+      </svg>
+      <span>{{ contractor.town }}, {{ contractor.country }}</span>
+    </div>
+
+    <div class="tcs-aside tcs-md" v-html="to_markdown(contractor.tag_line)"></div>
+
+    <div class="tcs-md" v-html="to_markdown(contractor.primary_description)"></div>
+
+    <div class="tcs-attr" v-for="attr in contractor_extra.extra_attributes">
+      <h3>{{ attr.name }}</h3>
+      <p v-if="attr.type == 'text_short' || attr.type == 'text_extended'" class="tcs-md" v-html="to_markdown(attr.value)"></p>
+      <p v-else>{{ attr.value }}</p>
+    </div>
+
+    <table class="tcs-skills" v-if="contractor_extra.skills">
+      <caption>
+        <h3>{{ $root.config.skills_label }}</h3>
+      </caption>
+      <tr v-for="skill in contractor_extra.skills">
+        <th scope="row">{{ skill.subject }}</th>
+        <td>
+        <span v-for="qual_level in filter_qual_levels(skill.qual_levels)">
+          {{ qual_level }}
+        </span>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+var utils = require('../utils')
+
+export default {
+  name: 'con-details',
+  methods: {
+    filter_qual_levels: (skills) => {
+      if (skills.length <= 5) {
+        return skills
+      } else {
+        return skills.slice(0, 2).concat(['...']).concat(skills.slice(-2))
+      }
+    },
+    to_markdown: utils.to_markdown
+  },
+  props: ['contractor'],
+  computed: {
+    contractor_extra: function () {
+      return this.$root.contractors_extra[this.$route.params.link] || {}
+    },
+  },
+  created: function () {
+    // TODO could do something less ugly here like hide the scroll bar at all times
+    this.body_overflow_before = document.body.style.overflow
+    document.body.style.overflow = 'hidden'
+  },
+  destroyed: function () {
+    document.body.style.overflow = this.body_overflow_before
+  }
+}
+</script>
+
+<style lang="scss">
+$hightlight: #1f2e50;
+
+.tcs-location {
+  margin-bottom: 10px;
+  float: right;
+  span {
+    display: inline-block;
+    padding-top: 4px;
+    vertical-align: top;
+    font-weight: 500;
+  }
+}
+
+.tcs-md {
+  p {
+    margin: 0 0 8px;
+  }
+}
+
+.tcs-aside {
+  font-size: 22px;
+  margin-bottom: 10px;
+  color: $hightlight;
+  min-height: 28px;
+}
+
+h3 {
+  margin-top: 12px;
+  margin-bottom: 4px;
+  font-size: 24px;
+  font-weight: 400;
+}
+
+table.tcs-skills {
+  border: none;
+  &, th, td, tr {
+    background-color: inherit;
+  }
+  caption {
+    text-align: left;
+  }
+  th, td {
+    padding: 0;
+    font-size: 15px;
+  }
+  tr {
+    height: 25px;
+  }
+  th {
+    text-align: left;
+    padding-right: 10px;
+    vertical-align: top;
+  }
+  td {
+    display: flex;
+    flex-wrap: wrap;
+    span {
+      white-space: nowrap;
+      padding: 3px 3px;
+      margin: 0 2px 2px;
+      color: white;
+      background: $hightlight;
+      border-radius: 3px;
+    }
+  }
+}
+</style>
--- src/components/modal.vue
+++ src/components/modal.vue
@@ -19,42 +19,7 @@
 
         <div class="tcs-body">
           <div class="tcs-content">
-            <div class="tcs-location">
-              <!--
-              this is the svg for map icon straight from
-              https://github.com/encharm/Font-Awesome-SVG-PNG/blob/master/black/svg/map-marker.svg
-              -->
-              <svg class="tcs-svg" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1152 640q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm256 0q0 109-33
-                  179l-364 774q-16 33-47.5 52t-67.5 19-67.5-19-46.5-52l-365-774q-33-70-33-179 0-212 150-362t362-150
-                  362 150 150 362z"/>
-              </svg>
-              <span>{{ contractor.town }}, {{ contractor.country }}</span>
-            </div>
-
-            <div class="tcs-aside tcs-md" v-html="to_markdown(contractor.tag_line)"></div>
-
-            <div class="tcs-md" v-html="to_markdown(contractor.primary_description)"></div>
-
-            <div class="tcs-attr" v-for="attr in contractor_extra.extra_attributes">
-              <h3>{{ attr.name }}</h3>
-              <p v-if="attr.type == 'text_short' || attr.type == 'text_extended'" class="tcs-md" v-html="to_markdown(attr.value)"></p>
-              <p v-else>{{ attr.value }}</p>
-            </div>
-
-            <table class="tcs-skills" v-if="contractor_extra.skills">
-              <caption>
-                <h3>{{ $root.config.skills_label }}</h3>
-              </caption>
-              <tr v-for="skill in contractor_extra.skills">
-                <th scope="row">{{ skill.subject }}</th>
-                <td>
-                <span v-for="qual_level in filter_qual_levels(skill.qual_levels)">
-                  {{ qual_level }}
-                </span>
-                </td>
-              </tr>
-            </table>
+            <con-details :contractor="contractor"></con-details>
           </div>
           <div class="tcs-extra">
             <img :src="contractor.photo" :alt="contractor.name">
@@ -72,22 +37,17 @@
 </template>
 
 <script>
-var utils = require('../utils')
+var con_details = require('./con-details')
 
 export default {
   name: 'tcs-modal',
   methods: {
     close: function () {
       this.$router.push({name: 'index'})
-    },
-    filter_qual_levels: (skills) => {
-      if (skills.length <= 5) {
-        return skills
-      } else {
-        return skills.slice(0, 2).concat(['...']).concat(skills.slice(-2))
-      }
-    },
-    to_markdown: utils.to_markdown
+    }
+  },
+  components: {
+    'con-details': con_details
   },
   computed: {
     contractor: function () {
@@ -97,9 +57,6 @@ export default {
           return contractor
         }
       }
-    },
-    contractor_extra: function () {
-      return this.$root.contractors_extra[this.$route.params.link] || {}
     },
     contact_html: function () {
       let raw = this.$root.config.contact_html
@@ -181,73 +138,11 @@ svg.tcs-svg {
   overflow-y: auto;
 }
 
-.tcs-location {
-  margin-bottom: 10px;
-  float: right;
-  span {
-    display: inline-block;
-    padding-top: 4px;
-    vertical-align: top;
-    font-weight: 500;
-  }
-}
-
-.tcs-md {
-  p {
-    margin: 0 0 8px;
-  }
-}
-
 .tcs-content {
   flex-grow: 1;
   padding-right: 10px;
   color: #444;
   width: calc(100% - 200px);
-  .tcs-aside {
-    font-size: 22px;
-    margin-bottom: 10px;
-    color: $hightlight;
-    min-height: 28px;
-  }
-  h3 {
-    margin-top: 12px;
-    margin-bottom: 4px;
-    font-size: 24px;
-    font-weight: 400;
-  }
-  table.tcs-skills {
-    border: none;
-    &, th, td, tr {
-      background-color: inherit;
-    }
-    caption {
-      text-align: left;
-    }
-    th, td {
-      padding: 0;
-      font-size: 15px;
-    }
-    tr {
-      height: 25px;
-    }
-    th {
-      text-align: left;
-      padding-right: 10px;
-      vertical-align: top;
-    }
-    td {
-      display: flex;
-      flex-wrap: wrap;
-      span {
-        white-space: nowrap;
-        padding: 3px 3px;
-        margin: 0 2px 2px;
-        color: white;
-        background: $hightlight;
-        border-radius: 3px;
-      }
-    }
-  }
 }
 
 $extra-width: 200px;
