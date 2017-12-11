e0014c8eb17f548c72e0fce2467a90d3ffea63a7
Samuel Colvin <s@muelcolvin.com>
2017-12-11 23:55:00 +0000

    fix tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  const config = {
+    mode: 'history'
+  }
+  ReactDOM.render(<Router><App config={config}/></Router>, div)
 })
