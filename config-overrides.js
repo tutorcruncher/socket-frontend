8e2798ecc255511b99098253ff45cbf98edde923
Samuel Colvin <s@muelcolvin.com>
2017-12-14 15:45:46 +0000

    tweaks and compress sass

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,32 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+
 module.exports = function override (config, env) {
   config.module.rules[1].oneOf.splice(config.module.rules[1].oneOf.length - 1, 0,
     {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'compressed'
+            }
+          }
+        ]
     },
   )
   if (env === 'production') {
     config.output.filename =  'static/socket.js'
     config.output.chunkFilename =  'static/socket.[chunkhash:8].chunk.js'
+    if (process.env.ANALYSE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: 'report.html',
+        })
+      )
+    }
   }
   // console.dir(config, { depth: 10, colors: true })
   return config
