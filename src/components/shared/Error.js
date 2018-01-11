44adcd7111d0d38c26ca6df89bc33099fca9b254
Samuel Colvin <s@muelcolvin.com>
2018-01-11 18:28:03 +0000

    tweak error page

--- src/components/shared/Error.js
+++ src/components/shared/Error.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
+const HELP_SITE = 'help.tutorcruncher.com/tc-socket/'
+
 const Error = ({children}) => (
   <div className="tcs-errors">
-    <p>
-      An error occurred with TutorCruncher socket:
-    </p>
+    <p>An error occurred while loading TutorCruncher socket:</p>
     <p className="tcs-error-content">{children}</p>
     <p>
-      It's likely that you've configured socket wrongly. You might get more information from the developer console, or
-      <a target="_blank" rel="noopener noreferrer" href="https://help.tutorcruncher.com/tc-socket/">help.tutorcruncher.com/tc-socket/</a>,
-      if you can't work out what's wrong contact support@tutorcruncher.com.
+      It's likely that you've configured socket wrongly. You might get more information from the developer console,
+      or <a target="_blank" rel="noopener noreferrer" href={'https://' + HELP_SITE}>{HELP_SITE}</a>,
+      if you still can't work out what's wrong contact support@tutorcruncher.com.
     </p>
   </div>
 )
